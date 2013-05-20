5d4da529dd68eef8bbcd10647fef03b7d5cd8ef1
fajarmuhf <fajarmuhf@gmail.com>
2013-05-20 16:13:15 +0700

    pert4

---
+++ Pertemuan-7/belajar-date/src/main/java/com/p2/belajardate/JenisProduk.java
@@ -0,0 +1,19 @@
+package com.p2.belajardate;
+
+public class JenisProduk{
+	private Integer id;
+	private String namaJenis;
+	
+	public Integer getId(){
+		return this.id;
+	}
+	public void setId(Integer id){
+		this.id = id;
+	}
+	public String getNamaJenis(){
+		return this.namaJenis;
+	}
+	public void setNamaJenis(String namaJenis){
+		this.namaJenis = namaJenis;
+	}
+}
---
+++ Pertemuan-7/belajar-date/src/main/java/com/p2/belajardate/Produk.java
@@ -0,0 +1,51 @@
+package com.p2.belajardate;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Produk{
+	private Integer id;
+	private String kodeProduk;
+	private JenisProduk jp;
+	private String nama;
+	private BigDecimal harga;
+	private Date waktuinput;
+	
+	public Integer getId(){
+		return this.id;
+	}
+	public void setId(Integer id){
+		this.id = id;
+	}
+	public String getKodeProduk(){
+		return this.kodeProduk;
+	}
+	public void setKodeProduk(String kodeProduk){
+		this.kodeProduk = kodeProduk;
+	}
+	public JenisProduk getJp(){
+		return this.jp;
+	}
+	public void setJp(JenisProduk jp){
+		this.jp = jp;
+	}
+	public String getNama(){
+		return this.nama;
+	}
+	public void setNama(String nama){
+		this.nama = nama;
+	}
+	public BigDecimal getHarga(){
+		return this.harga;
+	}
+	public void setHarga(BigDecimal harga){
+		this.harga = harga;
+	}
+	public Date getWaktuInput(){
+		return this.waktuinput;
+	}
+	public void setHarga(Date waktuinput){
+		this.waktuinput = waktuinput;
+	}
+	
+}
