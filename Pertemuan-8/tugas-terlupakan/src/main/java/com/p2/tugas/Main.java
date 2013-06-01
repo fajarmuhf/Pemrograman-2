9b2c026a0a06b803752e7a1753bbc795f8172798
fajarmuhf <fajarmuhf@gmail.com>
2013-06-01 23:11:20 +0700

    fajarmuhf

--- Pertemuan-8/tugas-terlupakan/src/main/java/com/p2/tugas/Main.java
+++ Pertemuan-8/tugas-terlupakan/src/main/java/com/p2/tugas/Main.java
@@ -13,6 +13,7 @@ import java.sql.ResultSet;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.text.ParseException;
+import java.lang.reflect.InvocationTargetException;
 
 public class Main 
 {
@@ -20,12 +21,14 @@ public class Main
     {
 		Scanner data = new Scanner(System.in);
 		int pilih = 0;
-		while(pilih != 3){
+		int pilih2 = 0;
+		while(pilih != 4){
 			System.out.println("=================");
 			System.out.println("MENU PILIHAN");
 			System.out.println("1. Tampil");
 			System.out.println("2. Input");
-			System.out.println("3. Keluar");
+			System.out.println("3. Pencarian");
+			System.out.println("4. Keluar");
 			System.out.print("Masukan Pilihan Anda : ");
 			pilih = data.nextInt();
 			data.nextLine();
@@ -72,6 +75,7 @@ public class Main
 					Date d = sdf.parse(oldDateString);
 					sdf.applyPattern(NEW_FORMAT);
 					newDateString = sdf.format(d);
+					
 					try{
 						Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tugas_terlupakan","root","");
 						String query = "INSERT INTO film(judul,tglRelease,rating,hargaFilm) values (?,?,"+rating+","+harga+")";
@@ -85,9 +89,90 @@ public class Main
 				}catch(ParseException pe){
 					
 				}
-				
-				
 				System.out.println("=================");
+			}
+			else if(pilih == 3){
+				System.out.println("=================");
+				System.out.print("Masukan Judul : ");
+				String cari = data.nextLine();
+				try{
+					Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/tugas_terlupakan","root","");
+					String sqlTampil = "SELECT * FROM film ";
+					PreparedStatement psTampil = c.prepareStatement(sqlTampil);
+					ResultSet rs = psTampil.executeQuery();
+					while(rs.next()){
+						if(cari.equals(rs.getString("judul"))){
+							System.out.println("=================");
+							System.out.println("id : "+rs.getInt("id"));
+							System.out.println("judul : "+rs.getString("judul"));
+							System.out.println("tanggal release : "+rs.getDate("tglRelease"));
+							System.out.println("rating : "+rs.getInt("rating"));
+							System.out.println("harga : "+rs.getDouble("hargaFilm"));
+							System.out.println("=================");
+							do{
+								System.out.println("MENU");
+								System.out.println("=================");
+								System.out.println("1.edit");
+								System.out.println("2.hapus");
+								System.out.println("3.keluar");
+								System.out.println("=================");
+								System.out.print("Pilih : ");
+								pilih2 = data.nextInt();
+								data.nextLine();
+								if(pilih2 == 1){
+									System.out.print("Masukan Judul : ");
+									String judul = data.nextLine();
+									System.out.print("Masukan Tanggal Release (DD-MM-YYYY): ");
+									String tanggal = data.nextLine();
+									System.out.print("Masukan Rating : ");
+									int rating = data.nextInt();
+									System.out.print("Masukan Harga Film : ");
+									BigDecimal harga = data.nextBigDecimal();
+									
+									try{					
+										final String OLD_FORMAT = "dd-MM-yyyy";
+										final String NEW_FORMAT = "yyyy-MM-dd";
+
+										String oldDateString = tanggal;
+										String newDateString;
+
+										SimpleDateFormat sdf = new SimpleDateFormat(OLD_FORMAT);
+										Date d = sdf.parse(oldDateString);
+										sdf.applyPattern(NEW_FORMAT);
+										newDateString = sdf.format(d);
+										
+										try{
+											Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tugas_terlupakan","root","");
+											String query = "UPDATE film SET judul='"+judul+"',tglRelease='"+newDateString+"',rating="+rating+",hargaFilm="+harga+" WHERE judul = '"+cari+"'";
+											PreparedStatement ps = conn.prepareStatement(query);
+											ps.executeUpdate();
+										}catch(SQLException se){
+											Logger.getLogger(Main.class.getName()).log(Level.SEVERE,null,se);
+										}
+									}catch(ParseException pe){
+										
+									}
+									System.out.println("=================");
+								}
+								else if(pilih2 == 2){
+									try{
+										Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tugas_terlupakan","root","");
+										String query = "DELETE FROM film WHERE judul = '"+cari+"'";
+										PreparedStatement ps = conn.prepareStatement(query);
+										System.out.println("Data "+cari+" dalam tabel film berhasil dihapus ");
+										ps.executeUpdate();
+									}catch(SQLException se){
+										Logger.getLogger(Main.class.getName()).log(Level.SEVERE,null,se);
+									}
+									System.out.println("=================");
+								}
+							}while(pilih2 != 3);
+						}
+					}
+					c.close();
+				}catch(SQLException se){
+					Logger.getLogger(Main.class.getName()).log(Level.SEVERE,null,se);
+				}
 			}
 		}
 		System.out.println("Terima Kasih Sudah Menggunakan Program Ini");
