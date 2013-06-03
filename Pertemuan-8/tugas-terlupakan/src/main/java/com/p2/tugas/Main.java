c51addd6cf77c14745af37bcde74291c1a3a5de4
fajarmuhf <fajarmuhf@gmail.com>
2013-06-03 15:41:15 +0700

    fajarmuhf

--- Pertemuan-8/tugas-terlupakan/src/main/java/com/p2/tugas/Main.java
+++ Pertemuan-8/tugas-terlupakan/src/main/java/com/p2/tugas/Main.java
@@ -1,5 +1,7 @@
 package com.p2.tugas;
 
+//mengimport library yang dibutuhkan untuk JDBC dengan menu pilihan
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -19,7 +21,9 @@ public class Main
 {
     public static void main( String[] args )
     {
+		//inisialisasi tipe data Scanner untuk menginput data ke variabel data
 		Scanner data = new Scanner(System.in);
+		//variabel untuk pemilihan
 		int pilih = 0;
 		int pilih2 = 0;
 		while(pilih != 4){
@@ -30,16 +34,24 @@ public class Main
 			System.out.println("3. Pencarian");
 			System.out.println("4. Keluar");
 			System.out.print("Masukan Pilihan Anda : ");
+			//menginput data ke variabel pilih
 			pilih = data.nextInt();
+			//untuk inputan digeser ke baris baru agar dapat menginput string dengan spasi
 			data.nextLine();
 			if(pilih == 1){
 				System.out.println("=================");
 				try{
+					//membuat koneksi ke database
 					Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/tugas_terlupakan","root","");
+					//string untuk query
 					String sqlTampil = "SELECT * FROM film ";
+					//menyiapkan query untuk dieksekusi
 					PreparedStatement psTampil = c.prepareStatement(sqlTampil);
+					//mengeksekusi query
 					ResultSet rs = psTampil.executeQuery();
+					//menampilkan tabel setelah query dieksekusi
 					System.out.println("|\tid\t|\tjudul\t|\ttglRelease\t|\trating\t|\thargaFilm\t|");
+					//perulangan untuk menampilkan isi dari tabel yang telah dieksekusi
 					while(rs.next()){
 						System.out.print("|\t"+rs.getInt("id")+"\t");
 						System.out.print("|\t"+rs.getString("judul")+"\t");
@@ -47,13 +59,16 @@ public class Main
 						System.out.print("|\t"+rs.getInt("rating")+"\t");
 						System.out.print("|\t"+rs.getDouble("hargaFilm")+"\t|\n");
 					}
+					//menutup koneksi
 					c.close();
 				}catch(SQLException se){
+					//membuang error sql 
 					Logger.getLogger(Main.class.getName()).log(Level.SEVERE,null,se);
 				}
 				System.out.println("=================");				
 			}
 			else if(pilih == 2){
+				//menginput judul,tanggal,rating,harga
 				System.out.println("=================");
 				System.out.print("Masukan Judul : ");
 				String judul = data.nextLine();
@@ -64,42 +79,57 @@ public class Main
 				System.out.print("Masukan Harga Film : ");
 				BigDecimal harga = data.nextBigDecimal();
 				
-				try{					
+				try{
+					//pendeklarasian format date dalam string					
 					final String OLD_FORMAT = "dd-MM-yyyy";
 					final String NEW_FORMAT = "yyyy-MM-dd";
 
+					//memasukan variabel tanggal ke oldDateString
 					String oldDateString = tanggal;
 					String newDateString;
 
+					//mengkonversi format dari format lama ke format baru
 					SimpleDateFormat sdf = new SimpleDateFormat(OLD_FORMAT);
 					Date d = sdf.parse(oldDateString);
 					sdf.applyPattern(NEW_FORMAT);
 					newDateString = sdf.format(d);
 					
 					try{
+						//membuat koneksi ke database
 						Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tugas_terlupakan","root","");
+						//menyiapkan query ke dalam string
 						String query = "INSERT INTO film(judul,tglRelease,rating,hargaFilm) values (?,?,"+rating+","+harga+")";
+						//menyiapkan query statement untuk dieksekusi
 						PreparedStatement ps = conn.prepareStatement(query);
+						//penginputan query pada setiap ?  
 						ps.setString(1,judul);
 						ps.setString(2,newDateString);
+						//mengeksekusi statement query
 						ps.executeUpdate();
 					}catch(SQLException se){
+						//menghandle error sql
 						Logger.getLogger(Main.class.getName()).log(Level.SEVERE,null,se);
 					}
 				}catch(ParseException pe){
-					
+					//menghandel error tanggal
 				}
 				System.out.println("=================");
 			}
 			else if(pilih == 3){
 				System.out.println("=================");
 				System.out.print("Masukan Judul : ");
+				//menginput data yang ingin dicari
 				String cari = data.nextLine();
 				try{
+					//membuat koneksi ke database
 					Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/tugas_terlupakan","root","");
+					//query dalam bentuk string
 					String sqlTampil = "SELECT * FROM film ";
+					//mempersiapkan query untuk dieksekusi
 					PreparedStatement psTampil = c.prepareStatement(sqlTampil);
+					//Hasil eksekusi query dimasukan kedalam variabel rs 
 					ResultSet rs = psTampil.executeQuery();
+					//menampilkan hasil query
 					while(rs.next()){
 						if(cari.equals(rs.getString("judul"))){
 							System.out.println("=================");
@@ -117,9 +147,12 @@ public class Main
 								System.out.println("3.keluar");
 								System.out.println("=================");
 								System.out.print("Pilih : ");
+								//menginput untuk pilihan ke 2 
 								pilih2 = data.nextInt();
+								//NextLine agar dapat memasukan String dengan spasi pada inputan berikutnya
 								data.nextLine();
 								if(pilih2 == 1){
+									//memasukan judul,tangal,rating,dan harga yang akan diubah menjadi yg diinput
 									System.out.print("Masukan Judul : ");
 									String judul = data.nextLine();
 									System.out.print("Masukan Tanggal Release (DD-MM-YYYY): ");
@@ -130,43 +163,59 @@ public class Main
 									BigDecimal harga = data.nextBigDecimal();
 									
 									try{					
+										
+										//pendeklarasian format date dalam string					
 										final String OLD_FORMAT = "dd-MM-yyyy";
 										final String NEW_FORMAT = "yyyy-MM-dd";
 
+										//memasukan variabel tanggal ke oldDateString
 										String oldDateString = tanggal;
 										String newDateString;
 
+										//mengkonversi format dari format lama ke format baru
 										SimpleDateFormat sdf = new SimpleDateFormat(OLD_FORMAT);
 										Date d = sdf.parse(oldDateString);
 										sdf.applyPattern(NEW_FORMAT);
 										newDateString = sdf.format(d);
 										
 										try{
+											//membuat koneksi ke database
 											Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tugas_terlupakan","root","");
+											//query dalam string
 											String query = "UPDATE film SET judul=?,tglRelease=?,rating=?,hargaFilm=? WHERE id = ?";
+											//mempersiapkan statement untuk dieksekusi
 											PreparedStatement ps = conn.prepareStatement(query);
+											//penginputan query pada setiap ?
 											ps.setString(1,judul);
 											ps.setDate(2,new java.sql.Date(d.getTime()) );
 											ps.setInt(3,rating);
 											ps.setBigDecimal(4,harga);
 											ps.setInt(5,rs.getInt("id"));
+											//mengeksekusi query
 											ps.executeUpdate();
 										}catch(SQLException se){
+											//membuang error sql
 											Logger.getLogger(Main.class.getName()).log(Level.SEVERE,null,se);
 										}
 									}catch(ParseException pe){
-										
+										//membuang error format date
 									}
 									System.out.println("=================");
 								}
 								else if(pilih2 == 2){
 									try{
+										//membuat koneksi ke database
 										Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/tugas_terlupakan","root","");
+										//query dalam string
 										String query = "DELETE FROM film WHERE judul = '"+cari+"'";
+										//membuat statement untuk dieksekusi
 										PreparedStatement ps = conn.prepareStatement(query);
+										//menampilkan keterangan data yang dicari sudah dihapus
 										System.out.println("Data "+cari+" dalam tabel film berhasil dihapus ");
+										//mengeksekusi query
 										ps.executeUpdate();
 									}catch(SQLException se){
+										//membuang error sql
 										Logger.getLogger(Main.class.getName()).log(Level.SEVERE,null,se);
 									}
 									System.out.println("=================");
@@ -174,8 +223,10 @@ public class Main
 							}while(pilih2 != 3);
 						}
 					}
+					//menutup koneksi database
 					c.close();
 				}catch(SQLException se){
+					//membuang error query
 					Logger.getLogger(Main.class.getName()).log(Level.SEVERE,null,se);
 				}
 			}
